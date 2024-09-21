2fe7a232f39d547667a42cd15de6e06460b42ade
Rahul Maurya <maurya.rahul682@gmail.com>
2024-09-21 11:33:24 +0530

    .

--- src/routes/tweet.routes.js
+++ src/routes/tweet.routes.js
@@ -3,6 +3,7 @@ import { verifyJWT } from "../middlewares/auth.middleware.js";
 import {
   createTweet,
   deleteTweet,
+  getAnyUserTweets,
   getUserTweets,
   updateTweet,
 } from "../controllers/tweet.controllers.js";
@@ -11,6 +12,7 @@ const router = Router();
 
 router.route("/create-tweet").post(verifyJWT, createTweet);
 router.route("/get-user-tweets").get(verifyJWT, getUserTweets);
+router.route("/get-any-user-tweets/:userId").get(verifyJWT, getAnyUserTweets);
 router.route("/update-tweet/:tweetId").patch(verifyJWT, updateTweet);
 router.route("/delete-tweet/:tweetId").delete(verifyJWT, deleteTweet);
 
